8dd6d4c9ab4dc435d6d565bc65f1897d52d70e81
Emiljan Dusha <emiljanthegreat@gmail.com>
2024-04-11 22:09:22 +0200

    stage 6 & refactor

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,49 @@
-use std::{io::Write, net::TcpListener};
+use std::{
+    io::Result as IOResult,
+    net::{TcpListener, TcpStream},
+    thread,
+};
 
 use crate::{
     request::{Method, Request},
-    response::StatusCode,
+    response::{Response, StatusCode},
 };
 
 mod request;
 mod response;
+
+fn handle_stream(mut stream: TcpStream) -> IOResult<usize> {
+    println!("accepted new connection");
+
+    let request = Request::build(&mut stream);
+    println!("{:?}", request);
+
+    match request.method {
+        Method::GET => {
+            if request.path.contains("/echo") {
+                let text = &request.path[6..];
+
+                Response::build(StatusCode::Ok, text.to_owned()).send(stream)
+            } else if request.path.contains("/user-agent") {
+                let user_agent = request
+                    .headers
+                    .iter()
+                    .find(|&header| header.contains("User-Agent"))
+                    .map(|s| s.split(":").last().unwrap().trim())
+                    .unwrap();
+
+                Response::build(StatusCode::Ok, user_agent.into()).send(stream)
+            } else if request.path == "/" {
+                Response::build(StatusCode::Ok, "".into()).send(stream)
+            } else {
+                Response::build(StatusCode::NotFound, "".into()).send(stream)
+            }
+        }
+        Method::POST => {
+            todo!("implement post")
+        }
+    }
+}
 
 fn main() {
     println!("Logs from your program will appear here!");
@@ -14,76 +51,8 @@ fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
 
     for stream in listener.incoming() {
-        if let Ok(mut stream) = stream {
-            println!("accepted new connection");
-
-            let request = Request::build(&mut stream);
-            println!("{:?}", request);
-
-            match request.method {
-                Method::GET => {
-                    if request.path.contains("/echo") {
-                        let text = &request.path[6..];
-                        stream
-                            .write(
-                                format!(
-                                    "HTTP/1.1 {} {}\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
-                                    StatusCode::Ok as usize,
-                                    StatusCode::Ok,
-                                    text.len(),
-                                    text
-                                )
-                                .as_bytes(),
-                            )
-                            .unwrap();
-                    } else if request.path.contains("/user-agent") {
-                        let user_agent = request
-                            .headers
-                            .iter()
-                            .find(|&header| header.contains("User-Agent"))
-                            .map(|s| s.split(":").last().unwrap().trim())
-                            .unwrap();
-
-                        stream
-                            .write(
-                                format!(
-                                    "HTTP/1.1 {} {}\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
-                                    StatusCode::Ok as usize,
-                                    StatusCode::Ok,
-                                    user_agent.len(),
-                                    user_agent
-                                )
-                                .as_bytes(),
-                            )
-                            .unwrap();
-                    } else if request.path == "/" {
-                        stream
-                            .write(
-                                format!(
-                                    "HTTP/1.1 {} {}\r\n\r\n",
-                                    StatusCode::Ok as usize,
-                                    StatusCode::Ok
-                                )
-                                .as_bytes(),
-                            )
-                            .unwrap();
-                    } else {
-                        stream
-                            .write(
-                                format!(
-                                    "HTTP/1.1 {} {}\r\n\r\n",
-                                    StatusCode::NotFound as usize,
-                                    StatusCode::NotFound
-                                )
-                                .as_bytes(),
-                            )
-                            .unwrap();
-                    }
-                }
-                Method::POST => {
-                    todo!("implement post")
-                }
-            }
+        if let Ok(stream) = stream {
+            thread::spawn(move || handle_stream(stream));
         }
     }
 }
--- src/response.rs
+++ src/response.rs
@@ -1,5 +1,10 @@
-use std::fmt::Display;
+use std::{
+    fmt::Display,
+    io::{self, Write},
+    net::TcpStream,
+};
 
+#[derive(Debug, Clone, Copy)]
 pub enum StatusCode {
     Ok = 200,
     NotFound = 404,
@@ -20,10 +25,29 @@ impl Display for StatusCode {
 
 #[derive(Debug)]
 pub struct Response {
-    pub status: usize,
+    pub status: StatusCode,
     pub body: String,
 }
 
 impl Response {
-    // pub fn build(status: usize, content: impl Buf) -> Self {}
+    pub fn build(status: StatusCode, body: String) -> Self {
+        Response { status, body }
+    }
+
+    pub fn send(self, mut stream: TcpStream) -> io::Result<usize> {
+        stream.write(self.to_string().as_bytes())
+    }
+}
+
+impl Display for Response {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "HTTP/1.1 {} {}\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
+            self.status as usize,
+            self.status,
+            self.body.len(),
+            self.body
+        )
+    }
 }
